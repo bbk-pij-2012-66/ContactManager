58cbc20778ac576a530a6ad41289d47427c84839
Baz iMac <baz@imac>
2013-03-12 20:41:10 +0000

    fleshed out contact implementation with name and id setters

--- src/Ass3/classes/ContactImpl.java
+++ src/Ass3/classes/ContactImpl.java
@@ -15,24 +15,75 @@ public class ContactImpl implements Contact
 	private String name;
 	private String notes;
 	
+	
+	/**
+	 * Returns the ID of the contact.
+	 *
+	 * @return the ID of the contact.
+	 */
 	@Override
 	public int getId()
 	{
 		return this.ContactID;
 	}
 
+	/**
+	 * Create an ID for the contact.
+	 *
+	 * Must be unique!
+	 */	
+	public void setId()
+	{
+		
+	}
+	
+	/**
+	 * Returns the name of the contact.
+	 *
+	 * @return the name of the contact.
+	 */
 	@Override
 	public String getName()
 	{
 		return this.name;
 	}
-
+	
+	/**
+	 * Set the name of the contact.
+	 * 
+	 * @param a string holding the contact, may not be empty
+	 */
+	public void setName(String name)
+	{
+		if (name == null || name.equals(""))
+		{
+			throw new IllegalArgumentException("This is not a valid name!");
+		}
+		else
+		{
+			this.name = name;
+		}
+	}
+	
+	/**
+	 * Returns our notes about the contact, if any.
+	 *
+	 * If we have not written anything about the contact, the empty
+	 * string is returned.
+	 *
+	 * @return a string with notes about the contact, maybe empty.
+	 */
 	@Override
 	public String getNotes() 
 	{
 		return this.notes;
 	}
 
+	/**
+	 * Add notes about the contact.
+	 *
+	 * @param note the notes to be added
+	 */
 	@Override
 	public void addNotes(String note) 
 	{
