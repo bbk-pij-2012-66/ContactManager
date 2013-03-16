bc9513ad1667b3eff1a0ede7b86822353cb694fc
Baz iMac <baz@imac>
2013-03-16 18:11:45 +0000

    save list of contacts to file

--- src/ass3/storage/ObjectToXML.java
+++ src/ass3/storage/ObjectToXML.java
@@ -1,11 +1,15 @@
 package ass3.storage;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 
 import ass3.classes.ContactImpl;
+import ass3.interfaces.Contact;
 
 public class ObjectToXML 
 {
@@ -13,15 +17,33 @@ public class ObjectToXML
 
 		String home = System.getProperty("user.home"); 
 		//		File file = new File(home + "/tmp/" +"file.xml");
-		File aFolder = new File(home + "/tmp/");
-		File aFile = new File(aFolder + "contacts.txt");
+		File aFolder = new File(home + "/tmp");
+		File aFile = new File(aFolder + "/contacts.txt");
 		
 		// test data
 		//------------------------
+		List<Contact> contacts = new ArrayList<Contact>();
+		
 		ContactImpl aContact = new ContactImpl();
 		aContact.setName("Baz Maj");
 		aContact.setId(1);
 		aContact.addNotes("junior java programmer");
+		contacts.add(aContact);
+		ContactImpl bContact = new ContactImpl();
+		bContact.setName("Matt Ross");
+		bContact.setId(2);
+		bContact.addNotes("junior c++ programmer");
+		contacts.add(bContact);
+		ContactImpl cContact = new ContactImpl();
+		cContact.setName("Pete Axe");
+		cContact.setId(2);
+		cContact.addNotes("java programmer");
+		contacts.add(cContact);
+		ContactImpl dContact = new ContactImpl();
+		dContact.setName("Pat Mas");
+		dContact.setId(4);
+		dContact.addNotes("senior java programmer");
+		contacts.add(dContact);
 		//------------------------
 
 		checkFolderExistsOtherwiseCreate(aFolder);
